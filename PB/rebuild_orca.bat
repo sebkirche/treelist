d284c01d7d47767146f4e444bbb7d841f874d4c2
Sébastien Kirche <sebastien.kirche@free.fr>
2014-06-05 10:44:27 +0200

    refactor of orca rebuild batches

--- PB/rebuild_orca.bat
+++ PB/rebuild_orca.bat
@@ -1,13 +1,14 @@
-echo off
-%~d0
-cd "%~dp0"
+@echo off
 
-set lib=TreeList105
-set pbver=105
+set pbver=%1
+set target=%2
+
+%~d0
+cd "%~dp0\src\%pbver%"
 
 echo Rebuild the pbl from sources
 echo.
-orcascr%pbver% /D targetname="%lib%" ..\..\rebuild_pbl_from_src.orca
+orcascr%pbver% /D targetname="%target%" ..\..\rebuild_pbl_from_src.orca
 echo.
 
 if errorlevel 1 goto failed
@@ -18,6 +19,9 @@ echo Process failed ?
 goto end:
 
 :ok
+copy /Y %target%.pbt ..\..\
+move /Y %target%.pbl ..\..\
+cd "%~dp0"
 echo Done. 
 echo %lib%.pbl should have been copied in the project directory.
 
