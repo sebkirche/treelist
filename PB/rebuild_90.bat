f005e8d715aa0c831c5bc98ef6add7798d1c26dd
Sébastien Kirche <sebastien.kirche@free.fr>
2014-10-16 12:21:30 +0200

    Fixed pbl rebuild with orca for both PB9 and PB10+

--- PB/rebuild_90.bat
+++ PB/rebuild_90.bat
@@ -1,34 +1,6 @@
 rem @echo off
 
-set target=TreeList90
-
 %~d0
-set logfile=%~dp0src\90\rebuild_%target%.log
+cd "%~dp0"
 
-set lpath=%~dp0
-if %lpath:~-1%==\ set lpath=%lpath:~0,-1%
-
-
-
-echo Rebuild the pbl from sources
-echo.
-orcascr9 /D targetname="%target%" /D lpath="%lpath%" /D ppath="%~dp0src\90" /D logfilename="%logfile%" rebuild_pbl_from_src_90.orca
-echo.
-
-if errorlevel 1 goto failed
-goto ok
-
-:failed
-echo Process failed ?
-goto end:
-
-:ok
-REM ~ attrib -R ..\..\%target%.pbt
-REM ~ copy /Y %target%.pbt ..\..\
-REM ~ move /Y %target%.pbl ..\..\
-REM ~ cd "%~dp0"
-echo Done. 
-echo %lib%.pbl should have been copied in the project directory.
-
-:end
-pause
+call rebuild_orca.bat 90 TreeList90
--- PB/rebuild_orca.bat
+++ PB/rebuild_orca.bat
@@ -4,11 +4,31 @@ set pbver=%1
 set target=%2
 
 %~d0
-cd "%~dp0\src\%pbver%"
+
+set logfile=%~dp0src\%pbver%\rebuild_%target%.log
+REM ~ set lpath=%~dp0
+REM ~ if %lpath:~-1%==\ set lpath=%lpath:~0,-1%
+REM for offline reconstruct, we need to work with all files in the same directory
+set lpath=".\\"
+
+REM ~ for the project path, the backslashes need to be doubled (WTF ?!!)
+set ppath=%~dp0src\%pbver%
+set ppath=%ppath:\=\\%
+
+REM ~ work in source directory
+cd %~dp0src\%pbver%
 
 echo Rebuild the pbl from sources
+echo working dir=%cd%
+echo lpath=%lpath%
+echo ppath=%ppath%
 echo.
-orcascr%pbver% /D targetname="%target%" ..\..\rebuild_pbl_from_src.orca
+if %pbver%==90 (
+	rem orcascr9 is a different from later versions, we need a specific script
+	orcascr9 /D targetname="%target%" /D lpath="%lpath%" /D ppath="%ppath%" /D logfilename="%logfile%" ..\..\rebuild_pbl_from_src_90.orca
+) else (
+	orcascr%pbver% /D targetname="%target%" /D lpath="%lpath%" /D ppath="%ppath%" /D logfilename="%logfile%" ..\..\rebuild_pbl_from_src.orca
+)
 echo.
 
 if errorlevel 1 goto failed
@@ -24,7 +44,7 @@ copy /Y %target%.pbt ..\..\
 move /Y %target%.pbl ..\..\
 cd "%~dp0"
 echo Done. 
-echo %lib%.pbl should have been copied in the project directory.
+echo %target%.pbl should have been copied in the project directory.
 
 :end
 pause
